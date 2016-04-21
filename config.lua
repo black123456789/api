2abfa3cce325d9b3e019692c5fbfc38c9ada1a50
black123456789 <jafariali22211@gmail.com>
2016-04-21 23:29:23 +0430

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 return {
-	bot_api_key = '193315453:AAEwoK0qTtbWALwJuNG1cufhGohM0igNQMI',
+	bot_api_key = '211467187:AAHA5t6NRq7m9PCf6ZvJ-lJrsxip7ZAtmJI',
 	time_offset = 0,
 	admin = 186861590,
 	channel = '',
